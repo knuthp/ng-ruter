d76eb43c69dd7cd7b3cf31713489081957f73634
Knut H Pedersen <knuthp@gmail.com>
2014-10-02 22:22:53 +0200

    Updating to devcenter version

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-app.use('/', express.static('build/'));
+app.use('/', express.static('bin/'));
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
