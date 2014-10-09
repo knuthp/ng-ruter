98d08649e5a4712d8695b4ded51558e12d946f21
Knut H Pedersen <p72@storebrand.no>
2014-10-09 21:39:42 +0200

    Starting with jasmine tests for filters

--- src/app/home/home.spec.js
+++ src/app/home/home.spec.js
@@ -7,22 +7,53 @@
 describe( 'home section', function() {
   beforeEach( module( 'ngRuter.home' ) );
 
- beforeEach(function () {
-    module('ngRuter.home');
+  describe('filters', function() {
+    beforeEach(function () {
+      inject(function (_$filter_) {
+        $filter = _$filter_;
+      });
+    });
 
-    inject(function (_$filter_) {
-      $filter = _$filter_;
+    it( 'should have a dummy test', inject( function() {
+      expect( true ).toBeTruthy();
+    }));
+
+
+    it( 'should have a fromNowFilter', function() {
+      var filter = $filter('fromNow');
+      expect(filter("")).toBe("a few seconds ago");
+    });
+
+
+    it( 'should have a isoInterval', function() {
+      var filter = $filter('isoInterval');
+      expect(filter("PT1H")).toBe("an hour");
+    });
+
+
+    it( 'should have a isoInterval that returns on time', function() {
+      var filter = $filter('isoInterval');
+      expect(filter("PT0S")).toBe("on time");
     });
   });
 
-  it( 'should have a dummy test', inject( function() {
-    expect( true ).toBeTruthy();
-  }));
+
+  describe('controller', function() {
+    var $rootScope, $scope, $controller, $httpBackend;
+
+    beforeEach(inject(function(_$rootScope_, _$controller_, _$timeout_, _$httpBackend_) {
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+      $controller = _$controller_;
+      $httpBackend = _$httpBackend_;
+
+      $timeout = _$timeout_;
 
 
-  it( 'should have a fromNowFilter', function() {
-    var filter = $filter('fromNow');
-    expect(filter("")).toBe("a few seconds ago");
+      $controller('myBusStopCtrl', {'$rootScope' : $rootScope, '$scope': $scope, $http : $httpBackend, $timeout : $timeout});
+
+
+    }));
   });
 });
 
